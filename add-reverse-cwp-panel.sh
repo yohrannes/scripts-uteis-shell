918124ac70ba6c72d6c797c3b034e71744c75a8c
root <root@node162287-yohrannes.jelastic.saveincloud.net>
2023-12-05 10:13:31 +0000

    updating script

--- add-reverse-cwp-panel.sh
+++ add-reverse-cwp-panel.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-read -p 'Digite o ip desejadp para criar o apontamento PTR reverso' ip
-cat $ip | grep .
-# cat /var/named | grep $4.$3.$2
+echo '[1] CWP panel'
+echo '[2] Cpanel'
+read -p 'Digite o tipo de painel de hospedagem:' tipopainel
+read -p 'Digite o dominio:' domain
+echo Será adicionado um apontamento reverso para o ip $1.$2.$3.$4 no dominio $domain
+if [ "$tipopainel" == "1" ]; then
+    # echo $4 14400 IN PTR $domain. >> /var/named/"$3"."$2"."$1"*
+    echo $4 14400 IN PTR $domain.  /var/named/"$3"."$2"."$1"*
+elif [ "$tipopainel" == "2" ]; then
+    # echo $4     14400   IN      PTR     $domain. >> /var/named/"$3"."$2"."$1"*
+    echo "$4     14400   IN      PTR     $domain.  /var/named/$3.$2.$1*"
+else
+    echo opção errada
+    sleep 2
+fi
+echo adicionando o seguinte PTR...
+cat /var/named/"$3"."$2"."$1"* | tail -1
