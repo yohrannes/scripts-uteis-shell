9ad3907a794261ae7fc248cd6b363fb40fed3c1d
yohrannes <51239326+yohrannes@users.noreply.github.com>
2023-12-27 10:42:06 -0300

    Update add-reverse-cwp-panel.sh

--- add-reverse-cwp-panel.sh
+++ add-reverse-cwp-panel.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-echo '[1] CWP panel'
-echo '[2] Cpanel'
-read -p 'Digite o tipo de painel de hospedagem:' tipopainel
-read -p 'Digite o dominio:' domain
-echo Será adicionado um apontamento reverso para o ip $1.$2.$3.$4 no dominio $domain
-if [ "$tipopainel" == "1" ]; then
-    echo $4 14400 IN PTR $domain. >> /var/named/"$3"."$2"."$1"*
-elif [ "$tipopainel" == "2" ]; then
-    echo "$4     14400   IN      PTR     $domain." >> /var/named/"$3"."$2"."$1"*
+
+echo Será adicionado um apontamento reverso para o ip $1.$2.$3.$4 no dominio $5
+if [ -d "/usr/local/cwpsrv" ]; then
+    echo $4 14400 IN PTR $5. >> /var/named/"$3"."$2"."$1"*
+elif [ -d "/usr/local/cpanel" ]; then
+    echo "$4     14400   IN      PTR     $5." >> /var/named/"$3"."$2"."$1"*
     /usr/local/cpanel/scripts/restartsrv_dnsadmin --restart --hard;/usr/local/cpanel/scripts/restartsrv_bind --restart --hard
 else
     echo Opção errada
