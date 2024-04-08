ae5215c9ff609ce95a0d07556e6cc3e8f0fec1ad
yohrannes <yohrannes@gmai.com>
2024-04-08 10:01:18 +0000

    Add feature to detect cpu architecture

--- auto-login-github-cli.sh
+++ auto-login-github-cli.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+ARCHITECTURE=$(hostnamectl | grep Arch | awk '{print $2}')
 VERSION=`curl  "https://api.github.com/repos/cli/cli/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-`
-wget https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_amd64.tar.gz
-tar xvf gh_${VERSION}_linux_amd64.tar.gz
-sudo cp gh_${VERSION}_linux_amd64/bin/gh /usr/local/bin/
+wget https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_${ARCHITECTURE}.tar.gz
+tar xvf gh_${VERSION}_linux_${ARCHITECTURE}.tar.gz
+sudo cp gh_${VERSION}_linux_${ARCHITECTURE}/bin/gh /usr/local/bin/
 gh version
 gh auth login
