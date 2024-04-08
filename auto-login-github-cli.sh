225978f4a6cc0f44aac84e6bb5c1010d3d8ce4e8
yohrannes <yohrannes@gmai.com>
2024-04-08 10:16:47 +0000

    fix get cpu arch feature

--- auto-login-github-cli.sh
+++ auto-login-github-cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ARCHITECTURE=$(hostnamectl | grep Arch | awk '{print $2}')
+ARCHITECTURE=$(hostnamectl status | grep Arch | awk '{print $2}');if [[ "${ARCHITECTURE}" == "x86-64" ]]; then ARCHITECTURE=amd64; fi;
 VERSION=`curl  "https://api.github.com/repos/cli/cli/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-`
 wget https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_${ARCHITECTURE}.tar.gz
 tar xvf gh_${VERSION}_linux_${ARCHITECTURE}.tar.gz
