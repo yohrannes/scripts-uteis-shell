4a8838a5c7941bb0d0ac2078bb3c9895d78d9024
yohrannes <yohrannes@gmail.com>
2024-04-11 19:11:47 -0300

    removing unused files

--- auto-login-github-cli.sh
+++ auto-login-github-cli.sh
@@ -2,7 +2,7 @@
 
 ARCHITECTURE=$(hostnamectl status | grep Arch | awk '{print $2}');if [[ "${ARCHITECTURE}" == "x86-64" ]]; then ARCHITECTURE=amd64; fi;
 VERSION=`curl  "https://api.github.com/repos/cli/cli/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-`
-wget https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_${ARCHITECTURE}.tar.gz
+wget "https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_${ARCHITECTURE}.tar.gz"
 tar xvf gh_${VERSION}_linux_${ARCHITECTURE}.tar.gz
 sudo cp gh_${VERSION}_linux_${ARCHITECTURE}/bin/gh /usr/local/bin/
 gh version
